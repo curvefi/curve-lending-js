1298e47c3eec5985cb70fac733e84f68990d86e9
macket <macket24@proton.me>
2024-02-12 16:29:44 +0400

    test: swap test

--- test/swap.test.ts
+++ test/swap.test.ts
@@ -1,56 +1,62 @@
-// import { assert } from "chai";
-// import { crvusd } from "../src/crvusd";
-// import { getLlamma, LlammaTemplate } from "../src/llammas";
-// import { BN } from "../src/utils";
-//
-// const LLAMMAS = ['sfrxeth'];
-//
-// const swapTest = (id: string) => {
-//     let llamma: LlammaTemplate;
-//     let maxDebt: string;
-//
-//     before(async function () {
-//         llamma = getLlamma(id);
-//         maxDebt = await llamma.createLoanMaxRecv(0.3, 10);
-//         await llamma.createLoan(0.3, maxDebt, 10)
-//     });
-//
-//     after(async function () {
-//         await llamma.fullRepay();
-//     });
-//
-//
-//     describe(`${id} llamma swap test`, function () {
-//         for (let i = 0; i < 2; i++) {
-//             for (let j = 0; j < 2; j++) {
-//                 if (i === j) continue;
-//
-//                 it(`${i} --> ${j}`, async function () {
-//                     const initialBalances = await llamma.wallet.balances();
-//                     const swapAmount = i === 0 ? Number(maxDebt) / 10 : 0.003;
-//                     const expected = Number(await llamma.swapExpected(i, j, swapAmount));
-//
-//                     await llamma.swap(i, j, swapAmount, 0.05);
-//
-//                     const balances = await llamma.wallet.balances();
-//                     const out = Number(Object.values(balances)[j]) - Number(Object.values(initialBalances)[j]);
-//
-//                     assert.deepStrictEqual(BN(Object.values(balances)[i]).toString(), BN(Object.values(initialBalances)[i]).minus(BN(swapAmount)).toString(), 'in');
-//                     assert.isAtMost(Math.abs(out - expected) / expected, 5 * 1e-3, 'out');
-//                 });
-//             }
-//         }
-//     });
-// }
-//
-// describe('Swap test', async function () {
-//     this.timeout(120000);
-//
-//     before(async function () {
-//         await crvusd.init('JsonRpc', {},{ gasPrice: 0 });
-//     });
-//
-//     for (const llammaId of LLAMMAS) {
-//         swapTest(llammaId);
-//     }
-// })
+import { assert } from "chai";
+import { lending } from "../src/lending.js";
+import { getOneWayMarket, OneWayMarketTemplate } from "../src/markets/index.js";
+import { BN } from "../src/utils.js";
+
+const ONE_WAY_MARKETS = ['one-way-market-0'];
+
+const swapTest = (id: string) => {
+    let oneWayMarket: OneWayMarketTemplate;
+    let maxDebt: string;
+
+    before(async function () {
+        oneWayMarket = getOneWayMarket(id);
+        const maxDeposit = Number(await oneWayMarket.vault.maxDeposit()) * 0.05;
+        await oneWayMarket.vault.deposit(maxDeposit);
+        maxDebt = await oneWayMarket.createLoanMaxRecv(0.3, 10);
+        await oneWayMarket.createLoan(0.3, maxDebt, 10)
+    });
+
+    after(async function () {
+        await oneWayMarket.fullRepay();
+    });
+
+
+    describe(`${id} one-way-market swap test`, function () {
+        for (let i = 0; i < 2; i++) {
+            for (let j = 0; j < 2; j++) {
+                if (i === j) continue;
+
+                it(`${i} --> ${j}`, async function () {
+                    const initialBalancesObj = await oneWayMarket.wallet.balances();
+                    const initialBalances = [initialBalancesObj.borrowed, initialBalancesObj.collateral];
+                    const swapAmount = i === 0 ? Number(maxDebt) / 10 : 0.003;
+                    const expected = Number(await oneWayMarket.swapExpected(i, j, swapAmount));
+
+                    await oneWayMarket.swap(i, j, swapAmount, 0.05);
+
+                    const balancesObj = await oneWayMarket.wallet.balances();
+                    const balances = [balancesObj.borrowed, balancesObj.collateral];
+                    const out = Number(balances[j]) - Number(initialBalances[j]);
+
+                    assert.deepStrictEqual(BN(balances[i]).toString(), BN(initialBalances[i]).minus(BN(swapAmount)).toString(), 'in');
+                    assert.isAtMost(Math.abs(out - expected) / expected, 5 * 1e-3, 'out');
+                });
+            }
+        }
+    });
+}
+
+describe('Swap test', async function () {
+    this.timeout(120000);
+
+    before(async function () {
+        await lending.init('JsonRpc', {},{ gasPrice: 0 });
+        await lending.fetchMarkets();
+    });
+
+    for (const oneWayMarketId of ONE_WAY_MARKETS) {
+        swapTest(oneWayMarketId);
+    }
+})
+
